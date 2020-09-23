c49265dfa6f37fdf4da1ad4829403b7438cac4f4
mikestu <mikestu2020@gmail.com>
2020-09-22 22:25:58 -0700

    Added model User.

---
+++ src/main/java/com/mike/course/app/user/User.java
@@ -0,0 +1,73 @@
+package com.mike.course.app.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Column
+    @NotBlank(message = "Surname is required")
+    private String surname;
+
+    @Column
+    @Email
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Column
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Column
+    @NotBlank(message = "Country is required")
+    private String country;
+
+    @Column
+    @NotBlank(message = "State is required")
+    private String state;
+
+    @Column
+    @NotBlank(message = "City is required")
+    private String city;
+
+    @Column
+    @NotBlank(message = "Gender is required")
+    private String gender;
+
+    @Column
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @Column
+    private Integer likes;
+
+    @Column
+    private String bio;
+
+    @Column
+    private LocalDate dateOfRegister;
+}
